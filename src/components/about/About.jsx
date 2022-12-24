0943917b1f457511588978c7826d5aadf82e8376
ikboljonradcode <ikboljon@radcode.co>
2022-12-24 20:19:56 +0100

    header photo is removed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img alt="About" />
+            <img src={ME} alt="About" />
           </div>
         </div>
         <div className="about__content">
